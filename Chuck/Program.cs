c92941e8e9f135c9158196e03c182a320b668696
Opilane TTHK <opilane@tthk.ee>
2020-01-17 09:13:24 +0200

    #

---
+++ Chuck/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Chuck
+{
+    class Program
+    {
+        static void Main()
+        {
+            string categoryURL = "https://api.chucknorris.io/jokes/categories";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(categoryURL);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream)) 
+            {
+                var response = responseReader.ReadToEnd();
+                WriteLine(response);
+            }
+        }
+    }
+}
